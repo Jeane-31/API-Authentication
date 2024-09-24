03fc9646632cd1cfeaff7a03472fa3ba7a9af90a
Navin <nnshri2000@gmail.com>
2024-09-24 12:27:32 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,70 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+//TODO 1: Fill in your values for the 3 types of auth.
+const yourUsername = "navinnn";
+const yourPassword = "Navin@123";
+const yourAPIKey = "53957acd-1a0b-4fce-b602-f21873f23041";
+const yourBearerToken = "0fa17982-b29b-4aac-b2be-0c5b2616153c";
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "API Response." });
+});
+
+app.get("/noAuth", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/random");
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.get("/basicAuth", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/all?page=2", {
+      auth: {
+        username: yourUsername,
+        password: yourPassword,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.get("/apiKey", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/filter", {
+      params: {
+        score: 5,
+        apiKey: yourAPIKey,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+const config = {
+  headers: { Authorization: `Bearer ${yourBearerToken}` },
+};
+
+app.get("/bearerToken", async (req, res) => {
+try{
+  const result = await axios.get(API_URL + "/secrets/2", config);
+  res.render("index.ejs", { content: JSON.stringify(result.data)});
+} catch(error){
+  res.status(404).send(error.message);
+}
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
